fd1951d981a0473371cb1666a1b9fb32dea8000a
Vaishakh Nambiar <vaishakhnambiar2002@gmail.com>
2021-12-15 13:44:38 +0530

    DAY15 update

--- work/webdev/node/usingExpress.js
+++ work/webdev/node/usingExpress.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express(); // creating an application
-/*
+
 app.get("/", (req, res, next) => {
   res.write("Using express");
   res.end();
@@ -13,16 +13,20 @@ app.get("/download", (req, res, next) => {
 app.get("/json", (req, res, next) => {
   res.json({ message: "Error " });
 });
-*/
+
 //-----------------------------
 
 //for rendering: we need a 'RENDER-ENGINE'
+
 app.set("view engine", "ejs");
 // and the file too should be with '.ejs' extension
-app.get("/", (req, res, next) => {
-  res.render("rendering");
+app.get("/render", (req, res, next) => {
+  res.render("rendering"); // by default it will be saved inside 'views' file
 });
-// by default it will be saved inside 'views' file
+
+app.get("/toFront", (req, res) => {
+  res.render("toFrontEnd", { text: "from backEnd through express" });
+});
 
 //---------------------------------
 
